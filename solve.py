3f7d31f6cd2d0bdf61bd8843808d2c44539f983f
FeverDreme <jeffreyhuang08@gmail.com>
2021-03-04 00:39:15 -0500

    bug fixes, docstrings

--- solve.py
+++ solve.py
@@ -4,26 +4,38 @@ from typing import List
 
 ListSizeError = Exception("List size must be even number")
 
-def magic(inputs, val):
-    inputs.append(val)
-    return inputs
-
 class Solver:
-    def __init__(self, inputs: List[int]):
+    def __init__(self, inputs: List[int], debug=0):
         self.mid, self.length, self.p1_range, self.p2_range, self.mids, self.inputs = Solver.resize_board(inputs)
 
-        print("Solver initialized")
+        # debug is the debug level, debug:0 is none, debug:1 is show initialization, debug:2 is everything
+        self.debug = debug
 
-        print()
-        print(f"mid: {self.mid}")
-        print(f"length: {self.length}")
-        print(f"p1_range: {self.p1_range}")
-        print(f"p2_range: {self.p2_range}")
-        print(f"inputs: {self.inputs}")
-        print()
+        print("Solver initialized\n")
+
+        if debug > 0:
+            print()
+            print(f"mid: {self.mid}")
+            print(f"length: {self.length}")
+            print(f"p1_range: {self.p1_range}")
+            print(f"p2_range: {self.p2_range}")
+            print(f"inputs: {self.inputs}")
+            print()
     
     @staticmethod
     def resize_board(board: List[int]):
+        """
+        Resizes board
+        :returns
+        [
+            _mid,
+            _length,
+            _p1_range,
+            _p2_range,
+            _mids,
+            _new_board
+        ]
+        """
         _length = len(board)
         if _length % 2: raise ListSizeError
 
@@ -44,6 +56,11 @@ class Solver:
         ]
 
     def next(self, pos: int, who: int):
+        """
+        Calculates the next position, accounts for cycles, skips on opponents pools\n
+        If who==1 then skips from 0 to 1\n
+        If who==0 then skips from mid to mid+1  
+        """
         ret = (pos + 1) % self.length
 
         if who == 1:
@@ -56,6 +73,10 @@ class Solver:
         return set(inputs[1:self.mid] + inputs[self.mid + 1:]) == set([0])
 
     def simulate(self, pos: int, inputs: List[int], player: int):
+        """
+        shape: [next_player, post-simulated board]
+        types: list[int, list[int]]
+        """
         # whos turn it is
         next_player = 1 - player # negates the player
 
@@ -87,30 +108,78 @@ class Solver:
             else:
                 break
 
-        return (next_player, inputs)
+        return [next_player, inputs]
 
     # minimax
-    def tree_search(self, inputs: List[int], maximizing_player: int, depth: int):
+    def tree_search(self, inputs: List[int], maximizing_player: int, depth: int, moves: List[int]):
+        """
+        input_shape: inputs, moves
+        output_shape(score): [score, moves]
+            case-maximizing_player==1: None, will get filtered out
+        """
+        if maximizing_player == -1:
+            return None
 
         # terminal case
-        if depth == 0 or maximizing_player == -1:
-            return inputs[self.mid] - inputs[0]
+        if depth == 0:
+            # print(inputs[self.mid] - inputs[0])
+            return (
+                inputs[self.mid] - inputs[0],
+                moves
+            )
         
         if maximizing_player == 1:
             possibilies = [self.simulate(pos=pos, inputs=inputs.copy(), player=maximizing_player) for pos in self.p1_range]
-            return max([self.tree_search(inputs=p[1], maximizing_player=p[0], depth=depth - 1) for p in possibilies])
+
+            searched = [self.tree_search(inputs=p[1], maximizing_player=p[0], depth=depth - 1, moves=moves + [ind+1]) for ind, p in enumerate(possibilies)]
+            searched = list(filter(lambda x: x != None, searched))
+            score = max(searched, key=lambda x: x[0])
         
         else:
             possibilies = [self.simulate(pos=pos, inputs=inputs.copy(), player=maximizing_player) for pos in self.p2_range]
-            return min([self.tree_search(inputs=p[1], maximizing_player=p[0], depth=depth - 1) for p in possibilies])
+
+            searched = [self.tree_search(inputs=p[1], maximizing_player=p[0], depth=depth - 1, moves=moves + [ind+1]) for ind, p in enumerate(possibilies)]
+            searched = list(filter(lambda x: x != None, searched))
+            score = min(searched, key=lambda x: x[0])
+        
+        if self.debug > 1:
+            print("searched:", searched)
+            print("depth", depth)
+            print("mp:", maximizing_player)
+            print("score", score, '\n')
+
+        return score
 
     def solve(self, depth: int = 1):
-        # simulate the first move
-        first_moves = [self.simulate(pos=pos, inputs=self.inputs.copy(), player=1) for pos in self.p1_range]
+        # set up the first moves
+        first_move = []
+        for pos in self.p1_range:
+            first_move.append(
+                (self.simulate(
+                        pos,
+                        self.inputs.copy(),
+                        1
+                    ), pos)
+            )
+        first_move = list(filter(lambda x: x[0][0] != -1, first_move))
 
-        outcomes = [self.tree_search(inputs=move[1], maximizing_player=move[0], depth=depth - 1) for move in first_moves]
-        best_outcome = max(outcomes)
+        outcomes = [
+            self.tree_search(
+                inputs=move[1],
+                maximizing_player=move[0],
+                depth=depth-1,
+                moves=[pos])
+            for move, pos in first_move
+        ]
 
+        best_outcome = max(outcomes, key=lambda x: x[0])
         # get correctsponding move
-        best_pos = outcomes.index(best_outcome) + 1 # +1 because 0-index
+        best_pos = best_outcome[1][0]
+
+        if self.debug > 0:
+            print(f"first_moves: {first_move}")
+            print(f"outcomes: {outcomes}")
+            print(f"best outcome: {best_outcome}")
+            print()
+
         return best_pos
